033ae62ac9203e4f593cb6371b40f9228d1fd85c
n1148884 <anton.ferreiradeandrade2022@my.ntu.ac.uk>
2025-11-12 16:59:56 +0000

    built HTTp client to fetch OSRM API response

--- COMP30231_CWK/src/java/Client.java
+++ COMP30231_CWK/src/java/Client.java
@@ -1,7 +1,11 @@
 
 import java.io.IOException;
+import java.lang.System.Logger.Level;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -22,6 +26,24 @@ public class Client {
         String options = "?overview=false";
         String url = baseUrl + coordinates + options;
         
-        System.out.println(url);
+        // create HTTP Client
+        HttpClient client = HttpClient.newHttpClient();
+        
+        URI uri = new URI(url);
+        
+        // build request
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(uri)
+            .GET()
+            .build();
+        
+        // send request and get response
+        HttpResponse<String> response;
+        response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() == 200) {
+            System.out.println(response.body());
+        } else {
+            System.out.println("Error" + response.statusCode());
+        }
     }
 }
