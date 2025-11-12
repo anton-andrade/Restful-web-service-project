ed5210bc994fb710e866d523f45af173d2a6c606
n1148884 <anton.ferreiradeandrade2022@my.ntu.ac.uk>
2025-11-12 16:24:54 +0000

    built and tested OSRM API URL with sample coordinates

--- COMP30231_CWK/src/java/Client.java
+++ COMP30231_CWK/src/java/Client.java
@@ -1,3 +1,8 @@
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -9,4 +14,14 @@
  */
 public class Client {
     
+    public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
+    
+        // build URI  
+        String baseUrl = "http://router.project-osrm.org/route/v1/driving/";
+        String coordinates = "13.388860,52.517037;13.397634,52.529407";
+        String options = "?overview=false";
+        String url = baseUrl + coordinates + options;
+        
+        System.out.println(url);
+    }
 }
