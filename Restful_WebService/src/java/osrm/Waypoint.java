7ff023ba0f00259c5ee6379a47fe49f7779f6f75
n1148884 <anton.ferreiradeandrade2022@my.ntu.ac.uk>
2025-11-27 16:33:50 +0000

    Initial commit: REST API project

---
+++ Restful_WebService/src/java/osrm/Waypoint.java
@@ -0,0 +1,39 @@
+package osrm;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author Antho
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Waypoint {
+
+    private List<Double> location;
+    private String name;
+
+    public Waypoint() {
+    }
+
+    public List<Double> getLocation() {
+        return location;
+    }
+
+    public void setLocation(List<Double> location) {
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
